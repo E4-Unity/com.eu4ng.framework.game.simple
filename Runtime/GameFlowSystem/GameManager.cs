c17236898d910f89a21297f1e76ddc43095bc11a
Eu4ng <eu4ng97@gmail.com>
2023-11-20 03:47:54 +0900

    feat(game-flow): Game Flow System 구현 - resolve #7
    ## 클래스
    
    - GameManager
    - StageManager

---
+++ Runtime/GameFlowSystem/GameManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Eu4ng.Framework.Game.GameFlowSystem
+{
+	public enum GameState
+	{
+		Init,
+		Enter,
+		Start,
+		End,
+		Exit
+	}
+
+	public abstract class GameManager : MonoBehaviour
+	{
+		/* 필드 */
+		bool _isPaused;
+		float _originalTimeScale;
+		GameState _gameState = GameState.Init;
+
+		float _playTime;
+		IEnumerator _gameTimer;
+
+		/* 프로퍼티 */
+		public bool IsPaused => _isPaused;
+		public GameState State => _gameState;
+
+		/* 이벤트 */
+		public event Action OnGameEnter;
+		public event Action OnGameStart;
+		public event Action OnGameEnd;
+		public event Action OnGameExit;
+
+		public event Action OnGamePause;
+		public event Action OnGameResume;
+
+		/* MonoBehaviour */
+		protected virtual void Awake()
+		{
+			_gameTimer = GameTimer();
+		}
+
+		/* API */
+		public void EnterGame()
+		{
+			if (_gameState != GameState.Init) return;
+			_gameState = GameState.Enter;
+
+			_playTime = 0;
+
+			OnGameEnter?.Invoke();
+			OnEnterGame();
+		}
+		public void StartGame()
+		{
+			if (_gameState != GameState.Enter) return;
+			_gameState = GameState.Start;
+
+			StartCoroutine(_gameTimer);
+
+			OnGameStart?.Invoke();
+			OnStartGame();
+		}
+
+		public void EndGame()
+		{
+			if (_gameState != GameState.Start) return;
+			_gameState = GameState.End;
+
+			StopCoroutine(_gameTimer);
+
+			OnGameEnd?.Invoke();
+			OnEndGame();
+		}
+
+		public void ExitGame()
+		{
+			if (_gameState != GameState.End) return;
+			_gameState = GameState.Exit;
+
+			OnGameExit?.Invoke();
+			OnExitGame();
+		}
+
+		public void PauseGame()
+		{
+			_isPaused = true;
+			OnGamePause?.Invoke();
+
+			_originalTimeScale = Time.timeScale;
+			Time.timeScale = 0f;
+
+			OnPauseGame();
+		}
+
+		public void ResumeGame()
+		{
+			_isPaused = false;
+			OnGameResume?.Invoke();
+
+			Time.timeScale = _originalTimeScale;
+
+			OnResumeGame();
+		}
+
+		/* 메서드 */
+		protected virtual void OnEnterGame(){}
+		protected virtual void OnStartGame(){}
+		protected virtual void OnEndGame(){}
+		protected virtual void OnExitGame(){}
+
+		protected virtual void OnPauseGame(){}
+		protected virtual void OnResumeGame(){}
+
+		IEnumerator GameTimer()
+		{
+			while (true)
+			{
+				_playTime += Time.deltaTime;
+				yield return null;
+			}
+		}
+	}
+}
---
+++ Runtime/GameFlowSystem/StageManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Eu4ng.Framework.Game.GameFlowSystem
+{
+	public enum StageState
+	{
+		Init,
+		Ready,
+		Start,
+		End
+	}
+	public abstract class StageManager : MonoBehaviour
+	{
+		/* 필드 */
+		StageState _stageState = StageState.Init;
+
+		float _playTime;
+		IEnumerator _stageTimer;
+
+		/* 프로퍼티 */
+		public StageState State => _stageState;
+		public float PlayTime => _playTime;
+
+		/* 이벤트 */
+		public event Action OnStageReady;
+		public event Action OnStageStart;
+		public event Action OnStageEnd;
+
+		/* MonoBehaviour */
+		protected virtual void Awake()
+		{
+			_stageTimer = StageTimer();
+		}
+
+		/* API */
+		public void ReadyStage()
+		{
+			if (_stageState is not (StageState.Init or StageState.End)) return;
+			_stageState = StageState.Ready;
+
+			_playTime = 0;
+
+			OnStageReady?.Invoke();
+			OnReadyStage();
+		}
+
+		public void StartStage()
+		{
+			if (_stageState != StageState.Ready) return;
+			_stageState = StageState.Start;
+
+			StartCoroutine(_stageTimer);
+
+			OnStageStart?.Invoke();
+			OnStartStage();
+		}
+
+		public void EndStage()
+		{
+			if (_stageState != StageState.Start) return;
+			_stageState = StageState.End;
+
+			StopCoroutine(_stageTimer);
+
+			OnStageEnd?.Invoke();
+			OnEndStage();
+		}
+
+		/* 메서드 */
+		protected virtual void OnReadyStage(){}
+		protected virtual void OnStartStage(){}
+		protected virtual void OnEndStage(){}
+
+		IEnumerator StageTimer()
+		{
+			while (true)
+			{
+				_playTime += Time.deltaTime;
+				yield return null;
+			}
+		}
+	}
+}
